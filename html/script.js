44489f7bd6699ea5335142d87b0622903acc455e
Jayasurya <96234368+JAYASURYA018@users.noreply.github.com>
2023-04-05 00:05:01 +0530

    Add files via upload

---
+++ html/script.js
@@ -0,0 +1,120 @@
+// function greet(name, lastname) {
+//   console.log("hello " + name + " " + lastname);
+// }
+
+// greet("jayasurya", "sai");
+
+// let x = 6;
+
+// if (x === 0) {
+//   console.log(`${x} is a odd number`);
+// } else {
+//   console.log(`${x} is a even number`);
+// }
+
+// for (i = 0; i <= 10; i++) {
+//   console.log(i);
+// }
+// console.log("print the odd numbers");
+// for (i = 0; i <= 99; i++) {
+//   if (i % 2 === 1) {
+//     console.log(i);
+//   }
+// }
+// console.log("factorial");
+// let num = 1000;
+// let factorial = 3;
+// for (i = 1; i <= num; i++) {
+//   factorial *= 1;
+// }
+// console.log(factorial);
+
+// // function handleclick() {
+// //   console.log("handle clicked");
+// // }
+
+// let handleclick = () => {
+//   console.log("handlecliked");
+// };
+
+// let arr = [1, 1, 2, 2, 3, 3, 4, 4];
+// let numbers = [10, 20, 30, 40, 50];
+
+// for (i = 0; i < numbers.length; i++) {
+//   console.log(numbers[i]);
+// }
+
+// let concatinate = arr.concat(numbers, "fruits", "apple");
+// console.log(concatinate);
+// console.log(numbers);
+
+// // sliced array
+
+// let fruits = ["apple", "mango", "orange", "grapes"];
+
+// let slicedarray = fruits.slice(1, 3);
+
+// console.log(slicedarray);
+
+// //Push a data in array
+
+// let newnum = [1, 2, 4, 5, 6, 7, 8];
+
+// let pushNewData = newnum.push(9, 10);
+
+// console.log(pushNewData);
+// console.log(newnum);
+
+// // fetch("https://jsonplaceholder.typicode.com/todos/1")
+// //   .then((response) => {
+// //     return response.json();
+// //   })
+// //   .then((data) => {
+// //     console.log(data);
+// //   });
+
+// // .catch((err) => {
+// //   console.log("err", err);
+// // });
+// // fetch("https://jsonplaceholder.typicode.com/todos/1")
+// //   .then((response) => response.json())
+// //   .then((json) => console.log(json));
+// // Fetch an Get API
+// // fetch("https://jsonplaceholder.typicode.com/users")
+// //   .then((response) => {
+// //     return response.json();
+// //   })
+// //   .then((data) => {
+// //     console.log("data", data);
+// //     data.forEach((user) => {
+// //       const userName = `<li> ${user.email}</li>`;
+// //       document.querySelector("ul").insertAdjacentHTML("beforeend", userName);
+// //     });
+// //   });
+
+// // fetch("https://jsonplaceholder.typicode.com/users")
+// //   .then((response) => {
+// //     return response.json();
+// //   })
+
+// //   .then((data) => {
+// //     console.log("data", data);
+// //     data.forEach((user) => {
+// //       const AllNames = `<li>${user.name}</li>`;
+// //       document.querySelector("ul").insertAdjacentHTML("beforeend", AllNames);
+// //     });
+// //   });
+// // fetch("https://jsonplaceholder.typicode.com/users")
+// //   .then((Response) => {
+// //     return Response.json();
+// //   })
+// //   .then((data) => {
+// //     data.forEach((user) => {
+// //       const Displayname = `<li>${user.email}</li>`;
+// //       console.log(Displayname);
+// //       document.querySelector("ul").insertAdjacentHTML("beforeend", Displayname);
+// //     });
+// //   });
+// // const names = ["jayasurya", "surya", "praveen", "sathya", "raghul"];
+
+// // names.forEach((name) => console.log(name));
